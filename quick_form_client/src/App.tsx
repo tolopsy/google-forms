c7c4b202aa9b2711458d51e0842c306b5bcff1fb
Tolulope Olanrewaju <lanre.toluwa@gmail.com>
2023-01-01 21:05:07 +0100

    first round of project setup

---
+++ quick_form_client/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <p className="tw-font-medium tw-py-8">Hello World</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ quick_form_client/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  prefix: "tw-",
+  content: [
+    "./src/**/*.{jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
