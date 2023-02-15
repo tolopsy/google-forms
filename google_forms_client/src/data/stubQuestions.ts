60e3dfecb6459d6e55adc277a86dfdfafe901e88
Tolulope Olanrewaju <lanre.toluwa@gmail.com>
2023-02-15 11:03:55 +0100

    chore: temporarily update stubQuestions

--- google_forms_client/src/data/stubQuestions.ts
+++ google_forms_client/src/data/stubQuestions.ts
@@ -13,7 +13,14 @@ const stubQuestions: Question[] = [
     type: QuestionType.MULTIPLE_CHOICE,
     options: ["1", "2", "3", "4", "5"],
     required: true,
-  }
+  },
+  {
+    id: "3",
+    text: "Rate our mobile app quality?",
+    type: QuestionType.MULTIPLE_CHOICE,
+    options: ["2", "4", "6", "8", "10"],
+    required: true,
+  },
 ]
 
 export default stubQuestions
