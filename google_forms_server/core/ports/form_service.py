f52b59dcbb2d77d1856fff01c9b9139b4d83e603
Tolulope Olanrewaju <lanre.toluwa@gmail.com>
2023-02-26 16:28:36 +0100

    feat: add form service method to handle form creation

--- google_forms_server/core/ports/form_service.py
+++ google_forms_server/core/ports/form_service.py
@@ -1,9 +1,14 @@
 from abc import ABC, abstractmethod
+from typing import Dict
 from .data_transfer import CreateQuestionInputDTO
 
 
 class FormServicePort(ABC):
     @abstractmethod
     def handle_save_question(self, question_input: CreateQuestionInputDTO) -> int:
+        pass
+
+    @abstractmethod
+    def handle_create_form(self) -> Dict:
         pass
 
--- google_forms_server/core/services/form_service.py
+++ google_forms_server/core/services/form_service.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass
-from core.models import Question
+from core.models import Question, form_factory
 from core.ports.db import DatabasePort
 from core.ports.data_transfer import CreateQuestionInputDTO
 from core.ports.form_service import FormServicePort
@@ -7,6 +7,12 @@ from core.ports.form_service import FormServicePort
 @dataclass
 class BasicFormService(FormServicePort):
     db: DatabasePort
+
+
+    def handle_create_form(self):
+        formData = self.db.create_and_return_new_form()
+        form = form_factory(**formData)
+        return form.to_dict()
 
     def handle_save_question(self, question_input: CreateQuestionInputDTO) -> int:
         question = Question(text=question_input.text)
