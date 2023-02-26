d3ed5d6916a1a87c8a77754a787aeec5c344a5e7
Tolulope Olanrewaju <lanre.toluwa@gmail.com>
2023-02-26 16:27:01 +0100

    feat: add db method to create and return new form data

--- google_forms_server/core/ports/db.py
+++ google_forms_server/core/ports/db.py
@@ -1,7 +1,12 @@
 from abc import ABC, abstractmethod
+from typing import Dict
 from core.models import Question
 
 class DatabasePort(ABC):
     @abstractmethod
     def save_question(self, question: Question) -> int:
+        pass
+    
+    @abstractmethod
+    def create_and_return_new_form(self) -> Dict:
         pass
--- google_forms_server/database/postgres/adapter.py
+++ google_forms_server/database/postgres/adapter.py
@@ -1,15 +1,30 @@
 import psycopg2
+from typing import Dict
 from core.ports.db import DatabasePort
-from core.models import Question
 
 
 class PostgresDB(DatabasePort):
     def __init__(self, dsn: str):
         self.conn = psycopg2.connect(dsn=dsn)
+    
+    def create_and_return_new_form(self) -> Dict:
+        query = "INSERT INTO form DEFAULT VALUES RETURNING *;"
+        with self.conn.cursor() as cursor:
+            cursor.execute(query)
+            self.conn.commit()
+            result = cursor.fetchone()
+            columns = cursor.description
 
-    def save_question(self, question: Question) -> Question:
+        keys = [col[0] for col in columns]
+        formData = dict(zip(keys, result))
+
+        print(formData)
+        return formData
+
+    # TODO: Remove this fake method
+    def save_question(self, question):
         
-        query = "INSERT INTO questions (text) VALUES (%s) RETURNING id;"
+        query = "INSERT INTO question (text) VALUES (%s) RETURNING id;"
         with self.conn.cursor() as cursor:
             cursor.execute(query, (question.text,))
             self.conn.commit()
