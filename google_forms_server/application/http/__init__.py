30bde292177d1fd4ec2affa05dd2c5fd60d93f53
Tolulope Olanrewaju <lanre.toluwa@gmail.com>
2023-02-26 16:30:29 +0100

    feat: add http handler for form creation

--- google_forms_server/application/http/__init__.py
+++ google_forms_server/application/http/__init__.py
@@ -1,9 +1,14 @@
 from core.ports.form_service import FormServicePort
 from .app import app
-from .adapter import FormView
+from .adapter import FormView, CreateFormView
 
 def run(form_service: FormServicePort, **kwargs):
-    app.add_url_rule("/", view_func=FormView.as_view("form_view", form_service=form_service))
+    app.add_url_rule(
+        "/forms/new/",
+        view_func=CreateFormView.as_view("create_form_view", form_service=form_service),
+        methods=["POST"]
+    )
+    app.add_url_rule("/fojjkdd", view_func=FormView.as_view("form_view", form_service=form_service))
 
     port = kwargs.get("port", 5000)
     host = kwargs.get("host", "localhost")
--- google_forms_server/application/http/adapter.py
+++ google_forms_server/application/http/adapter.py
@@ -1,5 +1,5 @@
 from flask import request, jsonify
-from flask.views import View
+from flask.views import View, MethodView
 from core.ports.form_service import FormServicePort
 from core.ports.data_transfer import CreateQuestionInputDTO
 
@@ -11,3 +11,9 @@ class FormView(View):
         data = request.get_json()
         id = self.form_service.handle_save_question(question_input=CreateQuestionInputDTO(text=data["text"]))
         return jsonify({"name": "Tolu", "id": id}), 201
+
+
+class CreateFormView(FormView):
+    def dispatch_request(self):
+        form_data = self.form_service.handle_create_form()
+        return jsonify(form_data)
