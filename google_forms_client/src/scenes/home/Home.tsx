990f0498491b27dc95d82238986bf509bb873470
Tolulope Olanrewaju <lanre.toluwa@gmail.com>
2023-02-26 17:15:33 +0100

    feat: handle creation of blank form

--- google_forms_client/src/scenes/home/Home.tsx
+++ google_forms_client/src/scenes/home/Home.tsx
@@ -1,12 +1,32 @@
+import {useCallback} from "react";
+import {useMutation} from "react-query";
 import MainHeader from "../../components/layouts/MainHeader";
 import Root from "../../components/layouts/Root";
 import TemplateOverview from "../../components/scenes/home/TemplateOverview";
+import createNewForm from "../../api/mutations/createNewForm";
+import {useNavigate} from "react-router";
+import {url} from "../../routing"
 
 export default function Home() {
+  const navigate = useNavigate();
+
+  const {mutate}  = useMutation(createNewForm, {
+    onSuccess: (data) => {
+      navigate(url("manageForm", {"formId": data.id}));
+    },
+    onError: (error) => {
+      console.log("Error: ", error);
+    }
+  })
+
+  const handleCreateBlankForm = useCallback(() => {
+   mutate()
+  }, [mutate])
+
   return (
     <Root>
       <MainHeader/>
-      <TemplateOverview/>
+      <TemplateOverview onCreateBlankForm={handleCreateBlankForm}/>
     </Root>
   )
 }
