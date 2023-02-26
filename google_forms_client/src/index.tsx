20829f4114d5cdbd4d257e0d0d414c845303965c
Tolulope Olanrewaju <lanre.toluwa@gmail.com>
2023-02-26 17:15:06 +0100

    feat: add query client provider

--- google_forms_client/src/index.tsx
+++ google_forms_client/src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {BrowserRouter} from "react-router-dom";
+import {QueryClient, QueryClientProvider} from "react-query";
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -9,10 +10,20 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    }
+  }
+});
+
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <QueryClientProvider client={queryClient}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </QueryClientProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
