55e04630fde286ea6c9979a4a2b79d0d9d3907b0
Tolulope Olanrewaju <lanre.toluwa@gmail.com>
2023-03-10 21:51:23 +0100

    feat: use connection pool and failover connection and add utility functions

--- google_forms_server/database/postgres/adapter.py
+++ google_forms_server/database/postgres/adapter.py
@@ -1,38 +1,62 @@
 import psycopg2
+from psycopg2.pool import ThreadedConnectionPool, PoolError
+from contextlib import contextmanager
 from typing import Dict
 from core.ports.db import DatabasePort
 
 
 class PostgresDB(DatabasePort):
     def __init__(self, dsn: str):
-        self.conn = psycopg2.connect(dsn=dsn)
+        self._connection_pool = ThreadedConnectionPool(minconn=1, maxconn=15, dsn=dsn)
+        self._failover_connection = psycopg2.connect(dsn=dsn)
+    
+    @contextmanager
+    def _get_connection(self):
+        using_failover = False
+        try:
+            conn = self._connection_pool.getconn()
+        except PoolError:
+            # TODO: Log pool error
+            conn = self._failover_connection
+            using_failover = True
+
+        try:
+            yield conn
+        finally:
+            if not using_failover:
+                self._connection_pool.putconn(conn)
+    
+    @contextmanager
+    def _get_cursor(self):
+        with self._get_connection() as connection:
+            with connection.cursor() as cursor:
+                yield cursor
     
     def create_and_return_new_form(self) -> Dict:
         query = "INSERT INTO form DEFAULT VALUES RETURNING *;"
-        with self.conn.cursor() as cursor:
+
+        with self._get_cursor() as cursor:
             cursor.execute(query)
-            self.conn.commit()
             result = cursor.fetchone()
             columns = cursor.description
 
         keys = [col[0] for col in columns]
         formData = dict(zip(keys, result))
 
-        print(formData)
         return formData
 
     # TODO: Remove this fake method
     def save_question(self, question):
         
         query = "INSERT INTO question (text) VALUES (%s) RETURNING id;"
-        with self.conn.cursor() as cursor:
+        with self._get_cursor() as cursor:
             cursor.execute(query, (question.text,))
-            self.conn.commit()
             id = cursor.fetchone()[0]
         
         return id
     
     def close(self):
-        if self.conn.closed == 0:
-            self.conn.close()
-    
+        if self._connection_pool.closed is False:
+            self._connection_pool.closeall()
+        if self._failover_connection.closed == 0:
+            self._failover_connection.close() 
