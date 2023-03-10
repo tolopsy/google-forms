ed4cb2bf8c35b72b6ce549ebdf12bd77f348def7
Tolulope Olanrewaju <lanre.toluwa@gmail.com>
2023-03-10 22:50:15 +0100

    feat: create server class and implement graceful exit

--- google_forms_server/server.py
+++ google_forms_server/server.py
@@ -1,24 +1,37 @@
 import os
+import sys
+from signal import signal, SIGINT, SIGTERM
 from dotenv import load_dotenv
 from database.postgres.adapter import PostgresDB
 from core.services.form_service import BasicFormService
 from application.http import run as run_http
 
 
-def get_db():
-    dsn = os.getenv("DB_DSN")
-    db = PostgresDB(dsn=dsn)
+# TODO: Clean messy entry point (maybe use a class)
 
-    return db
+class Server:
+    def __init__(self, db_dsn):
+        self.db = PostgresDB(dsn=db_dsn)
+        self.form_service = BasicFormService(db=self.db)
+        signal(SIGINT, self._exit_gracefully)
+        signal(SIGTERM, self._exit_gracefully)
+    
+    def _exit_gracefully(self, *args):
+        self.stop()
+        sys.stdout.flush()
+        sys.stderr.flush()
+        sys.exit()
+    
+    def run(self):
+        run_http(form_service=self.form_service)
+    
+    def stop(self):
+        self.db.close()
+
 
 if __name__ == "__main__":
     load_dotenv()
 
-    try:
-        db = get_db()
-    except Exception as err:
-        print(err)
-        exit()
-
-    form_service = BasicFormService(db=db)
-    run_http(form_service=form_service)
+    dsn = os.getenv("DB_DSN")
+    server = Server(db_dsn=dsn)
+    server.run()
